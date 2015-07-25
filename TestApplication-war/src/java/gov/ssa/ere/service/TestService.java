da2524ae62f2050094c14d551ae76f6253619e14
Lewis Isner <lewis.isner@gmail.com>
2015-07-25 14:52:45 -0400

    Added JSON and XML producers to RESTful web service

--- TestApplication-war/src/java/gov/ssa/ere/service/TestService.java
+++ TestApplication-war/src/java/gov/ssa/ere/service/TestService.java
@@ -6,9 +6,12 @@
 
 package gov.ssa.ere.service;
 
+import gov.ssa.ere.entity.Person;
+import java.util.Calendar;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Application;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -35,5 +38,29 @@ public class TestService {
     @Path("/git")
     public String git() {
         return "Hello there from git";
+    }
+
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person makePerson() {
+        return birthAPerson();
+    }
+
+    public Person birthAPerson() {
+        Person taylorSwift = new Person();
+        taylorSwift.setFirstName("Taylor");
+        taylorSwift.setLastName("Swift");
+        taylorSwift.setId(1);
+        taylorSwift.setDateOfBirth(Calendar.getInstance().getTime());
+        
+        return taylorSwift;        
+    }
+    
+    @GET
+    @Path("/xml")
+    @Produces(MediaType.APPLICATION_XML)
+    public Person makeAnotherPerson() {
+        return birthAPerson();
     }
 }
